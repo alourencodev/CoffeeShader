9c33236e2d23df3f817e6c85a8543d91618f98c4
André Lourenço <alourenco.dev@gmail.com>
2020-03-15 13:12:14 +0000

    Clean code

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main()
+static GLFWwindow *g_window = nullptr;
+
+static void init()
 {
     if (!glfwInit()) {
         std::cerr << "ERROR Loading GLFW" << std::endl;
         glfwTerminate();
-        return 1;
+        std::exit(1);
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -16,31 +18,46 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(1440, 1080, "Coffee Shader", NULL, NULL);
-    if (!window)
-    {
+    g_window = glfwCreateWindow(1440, 1080, "Coffee Shader", NULL, NULL);
+    if (!g_window) {
         std::cerr << "ERROR Creating GL Context" << std::endl;
         glfwTerminate();
-        return 1;
+        std::exit(1);
     }
 
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(g_window);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize OpenGL context" << std::endl;
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "ERROR Failed to initialize OpenGL context" << std::endl;
         glfwTerminate();
-        return 1;
+        std::exit(1);
     }
 
-    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-    }
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+}
 
+static void update()
+{
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(g_window);
+}
+
+static void terminate()
+{
+    glfwDestroyWindow(g_window);
     glfwTerminate();
+}
+
+int main()
+{
+    init();
+    
+    while (!glfwWindowShouldClose(g_window)) {
+        update();
+    }
+
+    terminate();
+
     return 0;
 }
