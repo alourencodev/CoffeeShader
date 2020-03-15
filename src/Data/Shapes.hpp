1908030da752a89de374d82af29030b47cec321a
André Lourenço <alourenco.dev@gmail.com>
2020-03-15 13:28:09 +0000

    Have hardcoded cube vertices

---
+++ src/Data/Shapes.hpp
@@ -0,0 +1,29 @@
+#ifndef COFFEE_SHAPES_HPP
+#define COFFEE_SHAPES_HPP
+
+#include <vector>
+
+namespace coffee::shapes
+{
+
+static const std::vector<float> k_cubeCoords = {
+    1.0f,1.0f,1.0f,    -1.0f,1.0f,1.0f,   -1.0f,-1.0f,1.0f,   1.0f,-1.0f,1.0f,    
+    1.0f,1.0f,1.0f,     1.0f,-1.0f,1.0f,   1.0f,-1.0f,-1.0f,  1.0f,1.0f,-1.0f,    
+    1.0f,1.0f,1.0f,     1.0f,1.0f,-1.0f,  -1.0f,1.0f,-1.0f,  -1.0f,1.0f,1.0f,     
+    -1.0f,-1.0f,-1.0f, -1.0f,1.0f,-1.0f,   1.0f,1.0f,-1.0f,   1.0f,-1.0f,-1.0f,   
+    -1.0f,-1.0f,-1.0f, -1.0f,-1.0f,1.0f,  -1.0f,1.0f,1.0f,   -1.0f,1.0f,-1.0f,    
+    -1.0f,-1.0f,-1.0f,  1.0f,-1.0f,-1.0f,  1.0f,-1.0f,1.0f,   -1.0f,-1.0f,1.0f  
+};
+
+static const std::vector<float> k_cubeFaceColors = {
+    1.0f,0.0f,0.0f,  1.0f,0.0f,0.0f,  1.0f,0.0f,0.0f,  1.0f,0.0f,0.0f,    
+    0.0f,1.0f,0.0f,  0.0f,1.0f,0.0f,  0.0f,1.0f,0.0f,  0.0f,1.0f,0.0f,     
+    0.0f,0.0f,1.0f,  0.0f,0.0f,1.0f,  0.0f,0.0f,1.0f,  0.0f,0.0f,1.0f,     
+    1.0f,1.0f,0.0f,  1.0f,1.0f,0.0f,  1.0f,1.0f,0.0f,  1.0f,1.0f,0.0f,     
+    0.0f,1.0f,1.0f,  0.0f,1.0f,1.0f,  0.0f,1.0f,1.0f,  0.0f,1.0f,1.0f,     
+    1.0f,0.0f,1.0f,  1.0f,0.0f,1.0f,  1.0f,0.0f,1.0f,  1.0f,0.0f,1.0f
+};
+
+}
+
+#endif
