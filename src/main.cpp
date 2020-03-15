1b46b71875fa7a05a5c1c9f77e57104698c02227
André Lourenço <alourenco.dev@gmail.com>
2020-03-15 00:44:24 +0000

    Setup gl environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "ERROR Loading GLFW" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1440, 1080, "Coffee Shader", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "ERROR Creating GL Context" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
