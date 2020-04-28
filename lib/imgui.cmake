34f685ace2a48ffbaf924368e77795162d8beac2
Andre Lourenco <alourenco.dev@gmail.com>
2020-04-28 20:43:54 +0100

    Integrate imgui library

---
+++ lib/imgui.cmake
@@ -0,0 +1,17 @@
+# Imgui
+
+cmake_minimum_required(VERSION 3.1)
+project(Imgui)
+
+SET(IMGUI_SRC
+    imgui/imgui.cpp
+    imgui/imgui_draw.cpp
+    imgui/imgui_widgets.cpp
+    imgui/examples/imgui_impl_glfw.cpp
+    imgui/examples/imgui_impl_opengl3.cpp
+)
+
+add_library(imgui ${IMGUI_SRC})
+add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
+target_link_libraries(imgui glad)
+target_include_directories(imgui PUBLIC imgui imgui/examples)
